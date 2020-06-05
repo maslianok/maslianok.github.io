1ac831fe79fa9bb2071c85a192f98207057c0228
Vitalii Maslianok <maslianok@gmail.com>
2020-06-05 15:13:34 +0300

    Update rsm example

---
+++ react-select-me/precache-manifest.4f23b68c6a992c61a0b8ed089ad45833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7392a699149372cf819245e14e73fccc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e03f992b02d67ecf29fe",
+    "url": "/static/css/main.f05c0ff3.chunk.css"
+  },
+  {
+    "revision": "6c47be2bcbefb72e4d3b",
+    "url": "/static/js/2.f0300df8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f0300df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e03f992b02d67ecf29fe",
+    "url": "/static/js/main.a2014ebd.chunk.js"
+  },
+  {
+    "revision": "6aec218a32ca873f1723",
+    "url": "/static/js/runtime-main.904694b2.js"
+  }
+]);
