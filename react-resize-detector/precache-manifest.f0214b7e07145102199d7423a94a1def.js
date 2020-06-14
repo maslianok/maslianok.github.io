87f1acf2058c89f7556f242c17d4d674afde0395
Vitalii Maslianok <maslianok@gmail.com>
2020-06-14 20:42:21 +0300

    Update rrd library

---
+++ react-resize-detector/precache-manifest.f0214b7e07145102199d7423a94a1def.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71b2fc35bd987916fac9080e4fdb8b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52f6c2ccfe9b683e6c91",
+    "url": "/static/js/2.00c590d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00c590d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c0900eeef7ac9937efd",
+    "url": "/static/js/main.635b0e0c.chunk.js"
+  },
+  {
+    "revision": "2cfd5c9ca41a186dc3e7",
+    "url": "/static/js/runtime-main.b93edf6a.js"
+  }
+]);
--- react-resize-detector/service-worker.js
+++ react-resize-detector/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34e486475214d5735e0863e2aa63e0a1.js"
+  "/precache-manifest.f0214b7e07145102199d7423a94a1def.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
